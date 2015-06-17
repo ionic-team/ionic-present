994d1d1f8ec6012d1be712e57ea22be533fcdf5a
perry <perry@drifty.com>
2015-06-17 13:11:37 -0500

    chicago

--- demos/list/index.js
+++ demos/list/index.js
@@ -1,27 +1,27 @@
 angular.module('ionicApp', ['ionic'])
 
 .controller('MyCtrl', function($scope) {
-  
+
   $scope.data = {
     showDelete: false
   };
-  
+
   $scope.edit = function(item) {
     alert('Edit Item: ' + item.id);
   };
   $scope.share = function(item) {
     alert('Share Item: ' + item.id);
   };
-  
+
   $scope.moveItem = function(item, fromIndex, toIndex) {
     $scope.items.splice(fromIndex, 1);
     $scope.items.splice(toIndex, 0, item);
   };
-  
+
   $scope.onItemDelete = function(item) {
     $scope.items.splice($scope.items.indexOf(item), 1);
   };
-  
+
   $scope.items = [
     { id: 0 },
     { id: 1 },
@@ -75,5 +75,5 @@ angular.module('ionicApp', ['ionic'])
     { id: 49 },
     { id: 50 }
   ];
-  
+
 });
